41dcc6e57190575304ff0f09f3dfe57991a9c78b
Rajat Verma <cserajatverma@gmail.com>
2025-11-12 13:55:29 +0530

    Add main Spring Boot application class
    Introduces SwaggerSpringHibernateMysqlApplication as the entry point for the Spring Boot application.

---
+++ src/main/java/com/technojerry/swagger_spring_hibernate_mysql/SwaggerSpringHibernateMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.technojerry.swagger_spring_hibernate_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSpringHibernateMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerSpringHibernateMysqlApplication.class, args);
+	}
+
+}
