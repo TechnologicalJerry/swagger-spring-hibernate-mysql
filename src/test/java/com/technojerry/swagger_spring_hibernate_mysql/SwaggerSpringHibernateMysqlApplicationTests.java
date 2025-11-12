aaa8c6e78d4d0ac54f22825229450420181f0bf3
Rajat Verma <cserajatverma@gmail.com>
2025-11-12 13:55:38 +0530

    Add basic Spring Boot context load test
    Introduces a JUnit test to verify that the application context loads successfully for the SwaggerSpringHibernateMysql application.

---
+++ src/test/java/com/technojerry/swagger_spring_hibernate_mysql/SwaggerSpringHibernateMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technojerry.swagger_spring_hibernate_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerSpringHibernateMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
